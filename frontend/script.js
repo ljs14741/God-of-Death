19d401966b67ead84ba4fc658a72d7d57a0a6eaa
binary <ljs14741@naver.com>
2024-01-25 23:34:34 +0900

    sns 공유하기 수정, ui 수정, 광고 추가

--- frontend/script.js
+++ frontend/script.js
@@ -114,13 +114,13 @@ function appendMessage(role, content) {
 }
 
 function shareTwitter() {
-    var sendText = "개발새발"; // 전달할 텍스트
-    var sendUrl = "devpad.tistory.com/"; // 전달할 URL
+    var sendText = "GodofDeath - 죽음 예측 테스트"; // 전달할 텍스트
+    var sendUrl = "https://godofdeath.pages.dev"; // 전달할 URL
     window.open("https://twitter.com/intent/tweet?text=" + sendText + "&url=" + sendUrl);
 }
 
 function shareFacebook() {
-    var sendUrl = "devpad.tistory.com/"; // 전달할 URL
+    var sendUrl = "https://godofdeath.pages.dev"; // 전달할 URL
     window.open("http://www.facebook.com/sharer/sharer.php?u=" + sendUrl);
 }
 
